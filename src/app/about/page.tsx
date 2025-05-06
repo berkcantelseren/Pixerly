27bb0019742e2efd4e39000463145b777f3bea83
Berkcan Telseren <berkcantelseren@gmail.com>
2025-05-06 23:51:52 +0300

    about page created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+import { Pixelify_Sans } from "next/font/google";
+
+const pixelify = Pixelify_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Pixelify",
+});
+
+export default function Home() {
+  return (
+    <div>
+      <h1
+        className={`${pixelify.className} font-semibold text-4xl select-none`}
+      >
+        About Page
+      </h1>
+    </div>
+  );
+}
