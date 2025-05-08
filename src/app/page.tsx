d5ebed947eb8d36d2d779eecae99d9f92bf053a0
Berkcan Telseren <berkcantelseren@gmail.com>
2025-05-08 23:37:00 +0300

    cleark sign in and sign up button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { Pixelify_Sans } from "next/font/google";
 
 const pixelify = Pixelify_Sans({
@@ -9,11 +16,19 @@ const pixelify = Pixelify_Sans({
 export default function Home() {
   return (
     <div>
-      <h1
-        className={`${pixelify.className} font-semibold text-4xl select-none`}
-      >
-        Pixerly
-      </h1>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
+
+// <h1
+//   className={`${pixelify.className} font-semibold text-4xl select-none`}
+// >
+//   Pixerly
+// </h1>
