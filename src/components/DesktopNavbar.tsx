601dce1635fe7a308ec7b3411c655959fbe4b37d
Berkcan Telseren <berkcantelseren@gmail.com>
2025-05-29 21:39:43 +0300

    navbar update

---
+++ src/components/DesktopNavbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DesktopNavbar() {
+  return <div></div>;
+}
+
+export default DesktopNavbar;
---
+++ src/components/MobileNavbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MobileNavbar() {
+  return <div></div>;
+}
+
+export default MobileNavbar;
